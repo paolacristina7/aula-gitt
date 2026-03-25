adfbe37a4cf27951a5874f574e9fdfb33ff6dbff
paolacristina7 <elacristina1988@gmail.com>
2026-03-25 15:30:13 -0300

    feat(ex07):aprovado ou reprovado

--- src/ex07.java
+++ src/ex07.java
@@ -1,5 +1,20 @@
-public class ex07 {
+
+
+    import java.util.Scanner;
+
+    public class ex07 {
+        void main(){
+            Scanner sc = new Scanner(System.in);
+            IO.println("Digite sua Nota Final: ");
+            double nota = sc.nextDouble();
+
+            if (nota >=7){
+                IO.println("Aprovado");
+            }else
+                IO.println("Reprovado");
+        }
+    }
 
 
 
-}
+
