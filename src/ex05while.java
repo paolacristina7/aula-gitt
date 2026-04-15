1e2546921157a2223af17543294cf047a81fdc3d
PAOLA CRISTINE SILVA BASTOS <paola.bastos@senaisp.edu.br>
2026-04-15 13:51:42 -0300

    feat (ex05while):exercicio5while)

---
+++ src/ex05while.java
@@ -0,0 +1,16 @@
+public class ex05while {
+    static void main() {
+                int contador = 0;
+
+                System.out.println("Números pares de 0 a 50:");
+
+
+                while (contador <= 50) {
+                    System.out.println(contador);
+
+
+                    contador += 2;
+                }
+            }
+        }
+
